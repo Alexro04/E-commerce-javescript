b929d7b329ec9575cf2998c67b3e7d5befe12602
Alexro04 <ajakatomi@gmail.com>
2024-12-18 22:46:01 +0100

    save Cart items in local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,4 @@
-export const cart = localStorage.getItem('cart') || [{
-  itemId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
-  quantity: 2 
-},
-{
-  itemId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', 
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 export function addToCart(itemId) {
   let sameProduct;
@@ -25,4 +18,12 @@ export function addToCart(itemId) {
       quantity: selectedQuantity
     })
   }
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+export function removefromCart(itemId){
+  cart = cart.filter((cartItem) => {
+    return cartItem.itemId !== itemId
+  })
+  localStorage.setItem('cart', JSON.stringify(cart))
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,56 +1,72 @@
-import {cart} from '../data/cart.js'
+import {cart, removefromCart} from '../data/cart.js'
 import {products} from '../data/products.js'
 
-let checkoutSummaryHTML;
+let checkoutSummaryHTML = '';
+
+function updatePage() {
+  if (cart.length === 0) {
+    document.querySelector('.js-main-checkout-section').innerHTML = '<h1>No Item in Your Cart</h1>'
+  }
+}
+
+updatePage()
 
 cart.forEach((cartItem) => {
   products.forEach((product) => {
     if (cartItem.itemId === product.id) {
-    checkoutSummaryHTML += `<div class="product-container">
-      <h2>Delivery date: Tuesday, June 21</h2>
-      <div class="product-grid">
-        <div class="image-container">
-          <img src='${product.image}' alt="" />
-        </div>
-        <div class="product-info-container">
-          <h3>${product.name}</h3>
-          <p class="product-price">$10.90</p>
-          <div class="quantity-container">
-            <p>Quantity: ${cartItem.quantity}</p>
-            <button>Update</button>
-            <button>Delete</button>
+      checkoutSummaryHTML += `<div class="product-container js-product-container-${product.id}">
+        <h2>Delivery date: Tuesday, June 21</h2>
+        <div class="product-grid">
+          <div class="image-container">
+            <img src='${product.image}' alt="" />
+          </div>
+          <div class="product-info-container">
+            <h3>${product.name}</h3>
+            <p class="product-price">$10.90</p>
+            <div class="quantity-container">
+              <p>Quantity: ${cartItem.quantity}</p>
+              <button>Update</button>
+              <button class='js-delete-item-button' data-delete-item=${product.id}>Delete</button>
+            </div>
+          </div>
+          <div class="delivery-options">
+            <h4>Choose a delivery option:</h4>
+            <div class="delivery-option">
+              <input type="radio" id="" name="delivery-date-${product.id}"/>
+              <label for="">
+                <p class="date">Tuesday, June 21</p>
+                <p>FREE Shipping</p>
+              </label>
+            </div>
+            <div class="delivery-option">
+              <input type="radio" id="" name="delivery-date-${product.id}"/>
+              <label for="">
+                <p class="date">Tuesday, June 21</p>
+                <p>FREE Shipping</p>
+              </label>
+            </div>
+            <div class="delivery-option">
+              <input type="radio" id="" name="delivery-date-${product.id}"/>
+              <label for="">
+                <p class="date">Tuesday, June 21</p>
+                <p>FREE Shipping</p>
+              </label>
+            </div>
           </div>
         </div>
-        <div class="delivery-options">
-          <h4>Choose a delivery option:</h4>
-          <div class="delivery-option">
-            <input type="radio" id="" name="delivery-date-${product.id}"/>
-            <label for="">
-              <p class="date">Tuesday, June 21</p>
-              <p>FREE Shipping</p>
-            </label>
-          </div>
-          <div class="delivery-option">
-            <input type="radio" id="" name="delivery-date-${product.id}"/>
-            <label for="">
-              <p class="date">Tuesday, June 21</p>
-              <p>FREE Shipping</p>
-            </label>
-          </div>
-          <div class="delivery-option">
-            <input type="radio" id="" name="delivery-date-${product.id}"/>
-            <label for="">
-              <p class="date">Tuesday, June 21</p>
-              <p>FREE Shipping</p>
-            </label>
-          </div>
-        </div>
-      </div>
-    </div>`
-}
-})
-
+      </div>`
+    }
+  })
 });
-console.log(checkoutSummaryHTML)
 
-document.querySelector('.all-products-container').innerHTML = checkoutSummaryHTML; 
+document.querySelector('.all-products-container').innerHTML = checkoutSummaryHTML;
+
+document.querySelectorAll('.js-delete-item-button')
+  .forEach((deleteButton) => {
+    const itemId = deleteButton.dataset.deleteItem
+    deleteButton.addEventListener('click', () => {
+      removefromCart(itemId)
+      document.querySelector(`.js-product-container-${itemId}`).remove()
+      updatePage()
+    })
+  })
